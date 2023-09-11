9b2a13e480cab05aca2dbaea9f1bc9a38baf51fe
mzaja <64711255+mzaja@users.noreply.github.com>
2023-09-11 20:20:27 +0100

    Add isort check to tests, update deploy script

--- scripts/test.bat
+++ scripts/test.bat
@@ -1,6 +1,8 @@
 @ECHO OFF
 black --check tests jacktrade
-IF %ERRORLEVEL% NEQ 0 ( EXIT /B 1 )
+IF %ERRORLEVEL% NEQ 0 ( EXIT 1 )
+isort --check tests jacktrade
+IF %ERRORLEVEL% NEQ 0 ( EXIT 1 )
 coverage run --source=jacktrade -m unittest discover -s tests -b
 coverage html
 coverage report
